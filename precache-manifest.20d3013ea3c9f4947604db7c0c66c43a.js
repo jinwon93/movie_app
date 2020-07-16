1b03f50f8ce3562ac8d4ea20fddc07d9ea7fe15e
username <kjw8208@nate.com>
2020-07-16 10:35:03 +0900

    Updates

---
+++ precache-manifest.20d3013ea3c9f4947604db7c0c66c43a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c6d190fad61aa7ec58cd0d371a55aa",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "6c2e1cd090fd91ee9eae",
+    "url": "/movie_app/static/css/main.0b23675f.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2e1cd090fd91ee9eae",
+    "url": "/movie_app/static/js/main.49da0321.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
