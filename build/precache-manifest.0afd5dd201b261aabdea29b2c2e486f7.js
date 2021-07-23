950b295a669b10260e133c922bde0deb39888a79
jinwon93 <kjw8208@nate.com>
2021-07-23 14:05:57 +0900

    tu_test_tdcc

---
+++ build/precache-manifest.0afd5dd201b261aabdea29b2c2e486f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "757ca70214f0719fb125555204239d60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "586132cee7bbfb018906",
+    "url": "/static/css/main.a95b8df8.chunk.css"
+  },
+  {
+    "revision": "ce02be54083148b5cf4f",
+    "url": "/static/js/2.508f3b55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.508f3b55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "586132cee7bbfb018906",
+    "url": "/static/js/main.970e105f.chunk.js"
+  },
+  {
+    "revision": "cf27343b5d386281fb60",
+    "url": "/static/js/runtime-main.1e9c0366.js"
+  }
+]);
